6e4f33b9a6c75b7b2507134c6a4fe16e499a1757
Forrest Collins <Forest.D.Collins@gmail.com>
2017-10-17 16:45:42 -0500

    A small cli implementation has been added

---
+++ src/cli.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const meow = require('meow');
+const chalk = require('chalk');
+
+const { Root, getConfig } = require('./config');
+const { buildClient, buildServer, cleanClient, cleanServer } = require('./commands/build');
+const { each } = require('./helpers/promise');
+
+const cli = meow(`
+	Usage:
+		$ frost <command>
+
+	Commands:
+		build
+		build:client
+		build:server
+		clean
+`);
+
+const input = command.input;
+const flags = command.flags;
+
+const tasks = [
+	{ task: 'clean', commands: [ cleanClient, cleanServer ]},
+	{ task: 'build', commands: [ cleanClient, cleanServer, buildClient, buildServer ]},
+	{ task: 'build:client', commands: [ cleanClient, buildClient ]},
+	{ task: 'build:server', commands: [ cleanServer, buildServer ]}
+];
+
+function execute(commands, config) {
+	return each(commands, (item) => item(config));
+}
+
+async function executeTasks() {
+	const config = await getConfig(flags);
+	for (const name of input) {
+		for (const task of tasks) {
+			if (task.task === name) {
+				try {
+					await execute(task.commands, config);
+				} catch (error) {
+					console.error(chalk.bold.red(`Failed to execute task: ${name}!`));
+					console.error(error);
+					process.exit(1);
+				}
+			}
+		}
+	}
+}
+
+if (input.length > 0) {
+	process.nextTick(executeTasks);
+} else {
+	command.showHelp();
+}
